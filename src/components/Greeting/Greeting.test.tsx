47efe00afd694a00a085885aa6018f2f07a4c639
Kok Chee Kean <kok_chee_kean@tech.gov.sg>
2020-06-12 18:44:34 +0800

    added some types for tests

--- src/components/Greeting/Greeting.test.tsx
+++ src/components/Greeting/Greeting.test.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import { Greeting } from './index';
 
 describe('<Greeting /> component test', function () {
   it('should render a default name of Bob', function () {
-    const component = mount(<Greeting />);
+    const component: ReactWrapper = mount(<Greeting />);
     expect(component.html()).to.have.string('Bob');
   });
 
   it('should render a name Alan given a name prop of Alan', function () {
-    const component = mount(<Greeting name="Alan" />);
+    const component: ReactWrapper = mount(<Greeting name="Alan" />);
     expect(component.html()).to.have.string('Alan');
   });
 });
